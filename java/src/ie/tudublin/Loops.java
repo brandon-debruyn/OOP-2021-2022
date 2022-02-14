19c253bd4804aa3b3f4c2b46005ac96e88fc17e7
brandon-debruyn <brandondebruyn123@gmail.com>
2022-02-14 11:47:41 +0000

    tut3

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -30,13 +30,51 @@ public class Loops extends PApplet
 	
 	public void draw()
 	{
+		background(0);
 		
 		switch(mode)	
 		{
 			case 0:
+			{
+				
+				float bars = (mouseX / 20.0f);
+				float w = width / bars;
+
+				for(int i=0; i<bars; i++)
+				{
+					noStroke();
+					fill(map(i, 0, bars, 0, 255), 255, 255);
+					rect(map(i, 0, bars, 0, 500), 0, w, height);
+				}
+				/*
+					map(a,b,c,d,e)
+					a = input val
+					b - c -> start and end of first range
+					d, e -> start and end of the range
+
+					return (a-b) / r1 * r2 
+					
+					(r1 = c-b)
+					(r2 = e-d)
+
+					map(-2, 10, 90, 200, 233);
+					r1 = (90 - 10) = 80
+					r2 = (233 - 200) = 33
+
+					200 + (-12 / 80) * 33 = 195.05
+				
+				*/
+				//map(-2, 10, 90, 200, 233);
 				break;
+			}
 			case 1:
+			{
+				noStroke();
+				
+				
 				break;
+			}
 		}
+
 	}
 }
