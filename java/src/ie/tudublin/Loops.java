9687664804c2decf97ec12259fb1a99c1ee7cdab
brandon-debruyn <brandondebruyn123@gmail.com>
2022-02-16 17:24:44 +0000

    bla

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -24,51 +24,39 @@ public class Loops extends PApplet {
 
 	float offset = 0;
 
+
+	/*
+		Draws a rectangle to the screen. A rectangle is a four-sided shape with every angle at ninety degrees. 
+		By default, the first two parameters set the location of the upper-left corner, the third sets the width, and the fourth 
+		sets the height.
+	*/
 	public void draw() {
 		switch (mode) {
 			case 0: {
 				background(0);
-				float bars = (mouseX / 20.0f);
-				float w = width / bars;
-
-				for (int i = 0; i < bars; i++) {
-					noStroke();
-					fill(map(i, 0, bars, 0, 255), 255, 255);
-					rect(map(i, 0, bars, 0, 500), 0, w, height);
+				noStroke();
+				float squares = (mouseX / 20.0f);
+				
+				for(int i = 0; i < squares; i++)
+				{
+					fill(map(i, 0, squares, 0, 255), 255, 255);
+					rect(map(i, 0, squares, 0, height), 0, width, height);
 				}
-				/*
-				 * map(a,b,c,d,e)
-				 * a = input val
-				 * b - c -> start and end of first range
-				 * d, e -> start and end of the range
-				 * 
-				 * return (a-b) / r1 * r2
-				 * 
-				 * (r1 = c-b)
-				 * (r2 = e-d)
-				 * 
-				 * map(-2, 10, 90, 200, 233);
-				 * r1 = (90 - 10) = 80
-				 * r2 = (233 - 200) = 33
-				 * 
-				 * 200 + (-12 / 80) * 33 = 195.05
-				 * 
-				 */
-				// map(-2, 10, 90, 200, 233);
+
 				break;
 			}
 			case 1: {
 				background(0);
-				float squares = (mouseX / 20.0f);
-				float h = (height / squares);
-
-				for (int i = 0; i < squares; i++) {
+				float squares = mouseX / 20.0f;
+				float h = height / squares;
+				for(int i=0; i<squares; i++)
+				{
 					noStroke();
-					// map i from 0 to squares and map the color 0 to 255
 					fill(map(i, 0, squares, 0, 255), 255, 255);
 					float x = map(i, 0, squares, 0, width);
 					rect(x, x, h, h);
-					rect((width - h) - x, x, h, h);
+					
+					
 				}
 
 				break;
@@ -76,21 +64,11 @@ public class Loops extends PApplet {
 			case 2:
 			{
 				background(0);
-				float circles = (mouseX / 20.0f);
-				offset += mouseY / 100;
-				float d = width / circles;
-				for(int j=0; j<circles; j++)
-				{
-					for(int i=0; i<circles ;i++)
-					{
-						noStroke();
-						float c = map((i + j + offset), 0, circles * 2, 0, 255) % 256;
-						fill(c, 255, 255);
-						float x = map(i, 0, circles -1, d/2.0f, width - (d/2.0f));
-						float y = map(j, 0, circles -1, d/2.0f, width - (d/2.0f));
-						circle(x, y, d);
-					}
-				}
+			}
+			case 3:
+			{
+				background(0);
+				
 			}
 		}
 
