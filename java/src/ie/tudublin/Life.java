fffe8c6e6e7f117ba4b3c2a157ef34e014098513
brandon-debruyn <brandondebruyn123@gmail.com>
2022-03-10 09:14:30 +0000

    gol lab

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -8,8 +8,8 @@ public class Life extends PApplet {
 
 
 	public void settings() {
-		size(1000, 1000);
-		board = new LifeBoard(200, this);
+		size(500, 500);
+		board = new LifeBoard(100, this);
 		board.randomise();
 		println(board.countCellsAround(1, 1));
 
